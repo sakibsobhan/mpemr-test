0b34a6f1988a5b267ad2ebc35d27be8664e6c3a7
Sakib Sobhan Khan <sakibsobhan@gmail.com>
2023-05-26 00:35:48 +0600

    news details page complete

--- src/components/NewsDetails/NewsDetails.js
+++ src/components/NewsDetails/NewsDetails.js
@@ -4,27 +4,33 @@ import thumbImg from '../../image/news-details-img.png'
 import searchIcon from '../../image/search-icon.svg'
 import smallThumb from '../../image/small-thumb-img.png'
 import SideLink from '../SideLink/SideLink';
+import shareIcon from '../../image/share-icon.svg';
 
 const NewsDetails = () => {
     return (
         <div className='py-[60px] lg:py-[120px]'>
             <div className="content-body">
-                <div className="news-title px-[110px]">
+                <div className="news-title md:px-[110px]">
                     <div className="meta flex gap-5 items-center">
-                        <p className='uppercase font-semibold text-[#555555]'>Our Blog</p>
+                        <a href='https://mpemr.gov.bd/news' className='uppercase font-semibold text-[#555555] hover:text-[#F42A41] duration-300'>Blog</a>
                         <span className='w-[5px] h-[5px] bg-[#D9D9D9] rounded-full'></span>
                         <p className='uppercase font-semibold text-[#555555]'>December 20, 2023</p>
                     </div>
-                    <h3 className='text-[#000225] font-semibold text-4xl leading-[44px] mt-[26px]' >Bangladesh Petroleum Corporation was established by Ordinance No. of 1976.</h3>
+                    <h3 className='text-[#000225] font-semibold text-2xl sm:text-4xl leading-[34px] sm:leading-[44px] mt-[26px]' >Bangladesh Petroleum Corporation was established by Ordinance No. of 1976.</h3>
                 </div>
-                <div className="thumbnail-img my-[60px] text-center">
-                    <img className='max-w-full' src={thumbImg} alt="" />
+                <div className="thumbnail-img my-[30px] sm:my-[60px] text-center">
+                    <img className='max-w-full max-h-[640px] object-cover' src={thumbImg} alt="" />
                 </div>
-                <div className="grid grid-cols-12 gap-[60px]">
-                    <div className="news-body col-span-8">
-                        <p className='text-[#707070] leading-[30px]'>So far 5 coal fields have been discovered in Bangladesh. The total reserves of coal in the 5 discovered coal fields are estimated at 3300 million tonnes. The use of coal as a fuel for power generation and brick kilns is gradually increasing. A mining lease was granted in 1994 by the Bureau of Mineral Resources Development in favor of Barapukuria Coal Mining Company Limited (BCMCL) for mining coal in Parvatipur Upazila of Dinajpur District. At present, coal is being extracted from the coalfield in the underground mining process (Under Ground Mining Process) and government revenue is being collected by selling the coal produced with 525 megawatt power generation in the power generation center located in the coal mining area. Besides, prospecting license was granted on 21.12.2008 in favor of Petrobangla for exploration of coal in Dighipara of Nawabganj Upazila of Dinajpur District. The said Exploration Agreement executed with Petrobangla has been transferred in favor of BCMCL on 21.10.2015 with a view to further development and acceleration of exploration activities. Currently feasibility study activities are underway by BCMCL to explore the coalfield. <br /> So far 5 coal fields have been discovered in Bangladesh. The total reserves of coal in the 5 discovered coal fields are estimated at 3300 million tonnes. The use of coal as a fuel for power generation and brick kilns is gradually increasing. A mining lease was granted in 1994 by the Bureau of Mineral Resources Development in favor of Barapukuria Coal Mining Company Limited (BCMCL) for mining coal in Parvatipur Upazila of Dinajpur District. At present, coal is being extracted from the coalfield in the underground mining process (Under Ground Mining Process) and government revenue is being collected by selling the coal produced with 525 megawatt power generation in the power generation center located in the coal mining area. Besides, prospecting license was granted on 21.12.2008 in favor of Petrobangla for exploration of coal in Dighipara of Nawabganj Upazila of Dinajpur District. The said Exploration Agreement executed with Petrobangla has been transferred in favor of BCMCL on 21.10.2015 with a view to further development and acceleration of exploration activities. Currently feasibility study activities are underway by BCMCL to explore the coalfield. </p>
+                <div className="lg:grid lg:grid-cols-12 gap-[60px]">
+                    <div className="news-body lg:col-span-8 mb-16 lg:mb-0">
+                        <div className="news-details-content">
+                            <p className='text-[#707070] leading-[30px]'>So far 5 coal fields have been discovered in Bangladesh. The total reserves of coal in the 5 discovered coal fields are estimated at 3300 million tonnes. The use of coal as a fuel for power generation and brick kilns is gradually increasing. A mining lease was granted in 1994 by the Bureau of Mineral Resources Development in favor of Barapukuria Coal Mining Company Limited (BCMCL) for mining coal in Parvatipur Upazila of Dinajpur District. At present, coal is being extracted from the coalfield in the underground mining process (Under Ground Mining Process) and government revenue is being collected by selling the coal produced with 525 megawatt power generation in the power generation center located in the coal mining area. Besides, prospecting license was granted on 21.12.2008 in favor of Petrobangla for exploration of coal in Dighipara of Nawabganj Upazila of Dinajpur District. The said Exploration Agreement executed with Petrobangla has been transferred in favor of BCMCL on 21.10.2015 with a view to further development and acceleration of exploration activities. Currently feasibility study activities are underway by BCMCL to explore the coalfield. <br /> So far 5 coal fields have been discovered in Bangladesh. The total reserves of coal in the 5 discovered coal fields are estimated at 3300 million tonnes. The use of coal as a fuel for power generation and brick kilns is gradually increasing. A mining lease was granted in 1994 by the Bureau of Mineral Resources Development in favor of Barapukuria Coal Mining Company Limited (BCMCL) for mining coal in Parvatipur Upazila of Dinajpur District. At present, coal is being extracted from the coalfield in the underground mining process (Under Ground Mining Process) and government revenue is being collected by selling the coal produced with 525 megawatt power generation in the power generation center located in the coal mining area. Besides, prospecting license was granted on 21.12.2008 in favor of Petrobangla for exploration of coal in Dighipara of Nawabganj Upazila of Dinajpur District. The said Exploration Agreement executed with Petrobangla has been transferred in favor of BCMCL on 21.10.2015 with a view to further development and acceleration of exploration activities. Currently feasibility study activities are underway by BCMCL to explore the coalfield. </p>
+                            <div className="share mt-10 sm:mt-20">
+                                <button className='flex gap-3 items-center bg-[#E6F6ED] text-[#07A74D] px-[25px] py-3 rounded-full'><img src={shareIcon} alt="" /> Share</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="side-bar col-span-4">
+                    <div className="side-bar lg:col-span-4">
                         <div className="search bg-[#F7F7F7] rounded-[10px] p-[30px] mb-[30px]">
                             <form className='relative' action="#">
                                 <input className='w-full h-[45px] rounded-md px-[15px] outline-none placeholder:text-[#8F8F8F] placeholder:font-semibold ' type="text" placeholder='Search'/>
--- src/components/SideLink/SideLink.js
+++ src/components/SideLink/SideLink.js
@@ -5,6 +5,32 @@ const SideLink = () => {
         <div>
             <div className="link-card bg-[#F7F7F7] rounded-[10px] p-[30px] mb-[30px]">
                 <h4 className='text-[#000225] font-semibold text-[21px] leading-none'>Information</h4>
+                <ul className='mt-[10px]'>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="http://pmo.gov.bd/">Prime Minister’s Office</a></li>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="http://www.parliament.gov.bd/">Bangladesh Parliament</a></li>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="https://a2i.gov.bd/">Access To Information (a2i)</a></li>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="https://bangladesh.gov.bd/index.php">National Web Portal of Bangladesh</a></li>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="http://www.bangladesh.gov.bd/site/view/all_eservices/">National E-Service</a></li>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="http://forms.mygov.bd/">All Forms of Service</a></li>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="http://services.portal.gov.bd/">Sebakunja</a></li>
+                    <li> <a className='block pt-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="https://bdpost.portal.gov.bd/">Post Code</a></li>
+                </ul>
+            </div>
+            <div className="link-card bg-[#F7F7F7] rounded-[10px] p-[30px] mb-[30px]">
+                <h4 className='text-[#000225] font-semibold text-[21px] leading-none'>Information</h4>
+                <ul className='mt-[10px]'>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="http://ocsms.desco.org.bd:8080/login">e-Services (DESCO)</a></li>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="https://www.bpdb.gov.bd/">Customer Care (BPDB)</a></li>
+                    <li> <a className='block pt-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="https://dpdc.gov.bd/site/page/c3697773-e03c-41ee-b1c8-043d7e7bf1db/Online-Bill-Payment-for-Post-Paid-Customer">Bill Payment (DPDC)</a></li>
+                </ul>
+            </div>
+            <div className="link-card bg-[#F7F7F7] rounded-[10px] p-[30px]">
+                <h4 className='text-[#000225] font-semibold text-[21px] leading-none'>General Report</h4>
+                <ul className='mt-[10px]'>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="https://bpdb.gov.bd/bpdb/index.php?option=com_content&view=article&id=126&Itemid=17">Maximum Generation</a></li>
+                    <li> <a className='block border-b-[1px] border-[#D9D9D9] py-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="https://bpdb.gov.bd/bpdb/index.php?option=com_content&view=article&id=20&Itemid=18">Daily Generation Archive</a></li>
+                    <li> <a className='block pt-3.5 text-[#8F8F8F] font-medium hover:text-[#F42A41] hover:pl-[5px] duration-300' href="https://bpdb.gov.bd/bpdb/index.php?option=com_content&view=article&id=151&Itemid=118">BPDB Daily Generation Report</a></li>
+                </ul>
             </div>
         </div>
     );
