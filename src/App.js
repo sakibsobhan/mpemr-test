87a3f5cd15fd4c63573a1e720674b6da094ff1c7
Sakib Sobhan Khan <sakibsobhan@gmail.com>
2023-05-29 23:28:41 +0600

    eservice added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-
-import './App.css';
-import About from './components/About/About';
-import Biography from './components/Biography/Biography';
-import Footer from './components/Footer/Footer';
-import Gallery from './components/Gallery/Gallery';
-import InnerBanner from './components/InnerBanner/InnerBanner';
-import NewsDetails from './components/NewsDetails/NewsDetails';
-import NewsSection from './components/NewsSection/NewsSection';
-import Officers from './components/Officers/Officers';
-import OfficersDetails from './components/OfficersDetails/OfficersDetails';
-import Partner from './components/Partner/Partner';
-import ProjectFeed from './components/ProjectFeed/ProjectFeed';
+import "./App.css";
+import About from "./components/About/About";
+import Biography from "./components/Biography/Biography";
+import Eservice from "./components/Eservice/Eservice";
+import Footer from "./components/Footer/Footer";
+import Gallery from "./components/Gallery/Gallery";
+import InnerBanner from "./components/InnerBanner/InnerBanner";
+import NewsDetails from "./components/NewsDetails/NewsDetails";
+import NewsSection from "./components/NewsSection/NewsSection";
+import Officers from "./components/Officers/Officers";
+import OfficersDetails from "./components/OfficersDetails/OfficersDetails";
+import Partner from "./components/Partner/Partner";
+import ProjectFeed from "./components/ProjectFeed/ProjectFeed";
 
 function App() {
   return (
     <div className="App">
-        <Partner></Partner>
-        <NewsSection></NewsSection>
-        <NewsSection></NewsSection>
-        <ProjectFeed></ProjectFeed>
-        <Footer></Footer>
-
-        <InnerBanner></InnerBanner>
-        <Gallery></Gallery>
-        <NewsDetails></NewsDetails>
-        <About></About>
-        <Biography></Biography>
-        <Officers></Officers>
-        <OfficersDetails></OfficersDetails>
+      <Partner></Partner>
+      <NewsSection></NewsSection>
+      <NewsSection></NewsSection>
+      <ProjectFeed></ProjectFeed>
+      <Footer></Footer>
+      <InnerBanner></InnerBanner>
+      <Gallery></Gallery>
+      <NewsDetails></NewsDetails>
+      <About></About>
+      <Biography></Biography>
+      <Officers></Officers>
+      <OfficersDetails></OfficersDetails>
+      <Eservice></Eservice>
     </div>
   );
 }
---
+++ src/components/Eservice/Eservice.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import SideLink from '../SideLink/SideLink';
+import eServiceLogo from '../../image/eservice-icon.png'
+
+const Eservice = () => {
+    return (
+        <div>
+            <div className="eService-page py-[60px] md:py-[120px]">
+                <div className="content_body">
+                    <div className="grid grid-cols-12 gap-[30px] lg:gap-[60px]">
+                        <div className="card-group col-span-12 md:col-span-8">
+                            <div className="grid grid-cols-12 gap-[30px]">
+                                <div className="card-item rounded-[10px] border-solid border-[1px] border-[#F7F7F7] overflow-hidden col-span-12 sm:col-span-6">
+                                    <div className="top flex justify-center py-[60px] bg-[#F7F7F7]">
+                                        <img className='w-[120px] h-[120px] object-cover' src={eServiceLogo} alt="" />
+                                    </div>
+                                    <div className="info p-[30px]">
+                                        <h3 className='text-[#000225] text-2xl font-semibold'>E-Documents Online</h3>
+                                        <p className='text-[#555555] text-[13px] uppercase font-semibold mt-2 mb-3'>Power Division</p>
+                                        <a className='text-[#07A74D] text-[13px] uppercase font-semibold' href={'google.com'}>Show Details</a>
+                                    </div>
+                                </div>
+                                <div className="card-item rounded-[10px] border-solid border-[1px] border-[#F7F7F7] overflow-hidden col-span-12 sm:col-span-6">
+                                    <div className="top flex justify-center py-[60px] bg-[#F7F7F7]">
+                                        <img className='w-[120px] h-[120px] object-cover' src={eServiceLogo} alt="" />
+                                    </div>
+                                    <div className="info p-[30px]">
+                                        <h3 className='text-[#000225] text-2xl font-semibold'>E-Documents Online</h3>
+                                        <p className='text-[#555555] text-[13px] uppercase font-semibold mt-2 mb-3'>Power Division</p>
+                                        <a className='text-[#07A74D] text-[13px] uppercase font-semibold' href={'google.com'}>Show Details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="links col-span-12 md:col-span-4">
+                            <SideLink></SideLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Eservice;
