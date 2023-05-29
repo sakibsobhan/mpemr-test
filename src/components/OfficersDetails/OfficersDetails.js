bc045fc191fd5fbc8942d34bb547a43069032e96
Sakib Sobhan Khan <sakibsobhan@gmail.com>
2023-05-29 23:51:03 +0600

    officers details correction

--- src/components/OfficersDetails/OfficersDetails.js
+++ src/components/OfficersDetails/OfficersDetails.js
@@ -7,32 +7,6 @@ const OfficersDetails = () => {
         <div>
             <div className="officer-details-part py-[60px] md:py-[120px]">
                 <div className="content_body">
-                    {/* <div className="grid items-center grid-cols-12 gap-[60px]">
-                    <div className="left col-span-12 sm:col-span-12 md:col-span-5">
-                    <div className="officer-image md:inline-block text-center bg-[#F7F7F7] py-6 md:py-0 rounded-[10px]">
-                        <img className='inline-block md:mt-[80px] md:ml-[80px] md:mb-[-80px] md:mr-[-80px] rounded-[10px] max-w-full md:max-w-[260px]' src={officer} alt="" />
-                    </div>
-                    </div>
-                    <div className="right col-span-12 sm:col-span-12 md:col-span-7">
-                    <div className="officer-details-info bg-[#F7F7F7] rounded-[10px] p-[30px] md:p-[60px]">
-                        <h3 className='text-4xl text-[#000225] font-semibold'>Mr. Md. Anichur Rahman</h3>
-                        <p className='text-[#555555] font-semibold uppercase mt-2'>Senior secretary (Energy)</p>
-                        <ul className='mt-6'>
-                            <li className='text-[#8F8F8F] mb-2'><span className='font-medium'>Name:</span> Mr. Md. Anichur Rahman (4610)</li>
-                            <li className='text-[#8F8F8F] mb-2'><span className='font-medium'>Designation:</span> Senior Secretary</li>
-                            <li className='text-[#8F8F8F] mb-2'><span className='font-medium'>Office:</span> Department of Energy and Mineral Resources</li>
-                            <li className='text-[#8F8F8F] mb-2'><span className='font-medium'>Email:</span> secretary@emrd.gov.bd</li>
-                            <li className='text-[#8F8F8F] mb-2'><span className='font-medium'>Mobile:</span> 01713-266373</li>
-                            <li className='text-[#8F8F8F] mb-2'><span className='font-medium'>Phone:</span> (Office) +08802-9514933</li>
-                            <li className='text-[#8F8F8F] mb-2'><span className='font-medium'>Intercom:</span> (Office) 101</li>
-                            <li className='text-[#8F8F8F] mb-2'><span className='font-medium'>Phone:</span> (Home) +8802-41031009</li>
-                            <li className='text-[#8F8F8F] mb-2'><span className='font-medium'>Fax:</span> +08802-9545110</li>
-
-
-                        </ul>
-                    </div>
-                    </div>
-                    </div> */}
                     <div className="md:flex items-center">
                     <div className="left md:w-[40%] mt-[-80px]">
                     <div className="officer-image md:inline-block text-center bg-[#F7F7F7] py-6 md:py-0 rounded-[10px]">
@@ -43,7 +17,7 @@ const OfficersDetails = () => {
                     <div className="officer-details-info bg-[#F7F7F7] rounded-[10px] p-[30px] md:p-[60px]">
                         <h3 className='text-2xl md:text-4xl text-[#000225] font-semibold'>Mr. Md. Anichur Rahman</h3>
                         <p className='text-[#555555] font-semibold uppercase mt-2'>Senior secretary (Energy)</p>
-                        <p className='text-[#8F8F8F] mt-6'>
+                        <p className='text-[#8F8F8F] mt-6 leading-9'>
                         নামঃ জনাব মোঃ আনিছুর রহমান (৪৬১০) <br />
 
                         পদবিঃ সিনিয়র সিচব <br />
