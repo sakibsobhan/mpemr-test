d40055a408f0e5891c41ce6bb06f8988c07cb4a4
Sakib Sobhan Khan <sakibsobhan@gmail.com>
2023-05-31 21:48:35 +0600

    eservice correction done

--- src/components/Eservice/Eservice.js
+++ src/components/Eservice/Eservice.js
@@ -10,22 +10,22 @@ const Eservice = () => {
                     <div className="grid grid-cols-12 gap-[30px] lg:gap-[60px]">
                         <div className="card-group col-span-12 md:col-span-8">
                             <div className="grid grid-cols-12 gap-[30px]">
-                                <div className="card-item rounded-[10px] border-solid border-[1px] border-[#F7F7F7] overflow-hidden col-span-12 sm:col-span-6">
+                                <div className="card-item hover:shadow-md duration-300 rounded-[10px] border-solid border-[1px] border-[#F7F7F7] overflow-hidden col-span-12 sm:col-span-6">
                                     <div className="top flex justify-center py-[60px] bg-[#F7F7F7]">
                                         <img className='w-[120px] h-[120px] object-cover' src={eServiceLogo} alt="" />
                                     </div>
                                     <div className="info p-[30px]">
-                                        <h3 className='text-[#000225] text-2xl font-semibold'>E-Documents Online</h3>
+                                        <h3 className='text-[#000225] text-[20px] font-semibold min-h-[60px]'>E-Documents Online</h3>
                                         <p className='text-[#555555] text-[13px] uppercase font-semibold mt-2 mb-3'>Power Division</p>
                                         <a className='text-[#07A74D] text-[13px] uppercase font-semibold' href={'google.com'}>Show Details</a>
                                     </div>
                                 </div>
-                                <div className="card-item rounded-[10px] border-solid border-[1px] border-[#F7F7F7] overflow-hidden col-span-12 sm:col-span-6">
+                                <div className="card-item hover:shadow-md duration-300 rounded-[10px] border-solid border-[1px] border-[#F7F7F7] overflow-hidden col-span-12 sm:col-span-6">
                                     <div className="top flex justify-center py-[60px] bg-[#F7F7F7]">
                                         <img className='w-[120px] h-[120px] object-cover' src={eServiceLogo} alt="" />
                                     </div>
                                     <div className="info p-[30px]">
-                                        <h3 className='text-[#000225] text-2xl font-semibold'>E-Documents Online</h3>
+                                        <h3 className='text-[#000225] text-[20px] font-semibold min-h-[60px]'>E-Documents Online</h3>
                                         <p className='text-[#555555] text-[13px] uppercase font-semibold mt-2 mb-3'>Power Division</p>
                                         <a className='text-[#07A74D] text-[13px] uppercase font-semibold' href={'google.com'}>Show Details</a>
                                     </div>
